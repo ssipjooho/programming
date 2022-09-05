beb8445f4a2d6a121596808393c27dba34c808ce
ssipjooho <112364469+ssipjooho@users.noreply.github.com>
2022-09-05 11:20:15 +0900

    Update for문.c

--- for문.c
+++ for문.c
@@ -8,7 +8,7 @@ int a;
   printf("===%d단===\n", a);
   for(int i = 1; i <= 9; i++)// (2nd) for문 설계 = 구구단 출력
     {
-     printf("%d*%d = %d\n", a, i, a*i);//서싯지정자를 활용하여 출력 
+     printf("%d*%d = %d\n", a, i, a*i);//서식지정자를 활용하여 출력 
     }
   return 0;
 }
