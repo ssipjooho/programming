418159c10269b6fb833f50a611054de24a40fb7c
ssipjooho <112364469+ssipjooho@users.noreply.github.com>
2022-09-05 11:18:32 +0900

    Create for문.c

---
+++ for문.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+int a;
+  
+  scanf("%d", &a); //(1st) 입력 받기
+  printf("===%d단===\n", a);
+  for(int i = 1; i <= 9; i++)// (2nd) for문 설계 = 구구단 출력
+    {
+     printf("%d*%d = %d\n", a, i, a*i);//서싯지정자를 활용하여 출력 
+    }
+  return 0;
+}
